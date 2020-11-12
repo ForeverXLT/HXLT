12dfd893c5f6a690026f9611fa14329568815100
ForeverXLT <71554379+ForeverXLT@users.noreply.github.com>
2020-11-12 09:52:46 +0800

    Add files via upload

---
+++ ForTest1.java
@@ -0,0 +1,14 @@
+public class ForTest1 {
+    public static void main(String[] args) {
+        /*for (int i = 0;i <= 100;){
+            i += 1;
+            System.out.println("我是" + i);
+        }
+        */
+        for (int i = 0; i <= 50; i++){
+            if(i % 2 != 0){
+                System.out.println("我是奇数" + i );
+            }
+        }
+    }
+}
